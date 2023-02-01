03b3cd961db76a4146795a3e3625c34c81f475b5
AymenXS <aymenghaloua@gmail.com>
2023-02-01 23:58:42 +0100

    Wrapping up

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const canvas = document.getElementById("canvas"),
   colorBtns = document.querySelectorAll(".colorField"),
   colorPicker = document.querySelector(".colorPicker"),
-  penWidth = document.querySelector(".penWidth");
+  penWidth = document.querySelector(".penWidth"),
+  clearButton = document.getElementById("clearButton"),
+  undoButton = document.getElementById("undoButton");
 
 canvas.width = window.innerWidth - 60;
 canvas.height = 400;
 
 let context = canvas.getContext("2d");
-context.fillStyle = "white";
+let startBackgroundColor = "white";
+context.fillStyle = startBackgroundColor;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 let drawColor = "black";
 let drawWidth = "2";
 let isDrawing = false;
 
+let restoreArray = [];
+let index = -1;
+
 canvas.addEventListener("mousedown", start, false);
 canvas.addEventListener("mousemove", draw, false);
 canvas.addEventListener("mouseup", stop, false);
 canvas.addEventListener("mouseout", stop, false);
+
+colorBtns.forEach((button) => {
+  button.addEventListener("click", changeColor);
+});
+
+colorPicker.addEventListener("input", changeColorPicker);
+penWidth.addEventListener("input", changeWidth);
+undoButton.addEventListener("click", undoLast);
+clearButton.addEventListener("click", clearCanvas);
 
 function start(event) {
   isDrawing = true;
@@ -35,22 +50,20 @@ function draw(event) {
   event.preventDefault();
 }
 
-function stop() {
+function stop(event) {
   if (isDrawing) {
     context.stroke();
     isDrawing = false;
   }
+
+  if (event.type != "mouseout") {
+    restoreArray.push(context.getImageData(0, 0, canvas.width, canvas.height));
+    index += 1;
+  }
 }
 
-colorBtns.forEach((button) => {
-  button.addEventListener("click", changeColor);
-});
-
-colorPicker.addEventListener("input", changeColorPicker);
-penWidth.addEventListener("input", changeWidth);
-
 function changeColor() {
-  drawColor = this.style.background;
+  drawColor = getComputedStyle(this).backgroundColor;
 }
 
 function changeColorPicker() {
@@ -59,4 +72,23 @@ function changeColorPicker() {
 
 function changeWidth() {
   drawWidth = this.value;
+}
+
+function clearCanvas() {
+  context.fillStyle = startBackgroundColor;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  restoreArray = [];
+  index = 1;
+}
+
+function undoLast() {
+  if (index <= 0) {
+    clearCanvas();
+  } else {
+    index -= 1;
+    restoreArray.pop();
+    context.putImageData(restoreArray[index], 0, 0);
+  }
 }
